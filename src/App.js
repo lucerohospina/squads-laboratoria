035f9241750ee646ed0bd06c08d197d454903d67
lucerohospina <lucerojhp@gmail.com>
2018-03-27 21:07:14 -0500

    fixing merging bugs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Header from './components/Header';
-import firebase from 'firebase';
-import Login from './components/Login';
-import Footer from './components/Footer';
-import './App.css';
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    var config = {
-      apiKey: "AIzaSyA0KGnze8Q2eeKLoxWNINrS-zuDe-Dzm5w",
-      authDomain: "telmexhub-5da16.firebaseapp.com",
-      databaseURL: "https://telmexhub-5da16.firebaseio.com",
-      projectId: "telmexhub-5da16",
-      storageBucket: "telmexhub-5da16.appspot.com",
-      messagingSenderId: "360942649813"
-    };
-    firebase.initializeApp(config);
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Login db={ firebase }/>
-        <Footer/>
-      </div>
-    );
-  }
-=======
 import React from 'react';
 import Home from './components/home';
 import Header from './components/Header';
@@ -56,7 +24,6 @@ const App = (props) => {
 
 		</div>
 	</BrowserRouter>)
->>>>>>> 6195d725e4d5e287b672dd836c4ef5d23fb511c2
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const message= (db) =>{
-    //console.log(db.database().ref('sprint-notes/'));
-    let email = document.getElementsByClassName('loginEmail')[0].value;
-    let password = document.getElementsByClassName('loginPassword')[0].value;
-
-    const dbRefObject = db.database().ref();
-    dbRefObject.on('value', function(snap) {
-      console.log(snap.val());
-    })
-		
-		if(email !== "" && password !== ""){
-			//$('#loginProgress').show();
-			db.auth().signInWithEmailAndPassword(email, password).catch(function(error){
-        alert(error.message);
-       
-				//$('#loginError').show().text(error.message);
-				//$('#loginProgress').hide();
-			})
-		}
-}
-
-const Login = ({db}) => {
-  // functions here
- 
-
-    return (
-      <div className="row margin-top">
-      <form className="col s12">
-        <div className="row">
-          <div className="input-field col s12 m6 offset-m3">
-            <i className="material-icons prefix">email</i>
-            <input id="icon_prefix" type="text" className="validate loginEmail"/>
-            <label htmlFor="icon_prefix">correo electrónico</label>
-          </div>
-        </div>
-        <div className="row">
-        <div className="input-field col s12 m6 offset-m3">
-          <i className="material-icons prefix">create</i>
-          <input id="icon_telephone" type="tel" className="validate loginPassword"/>
-          <label htmlFor="icon_telephone">contraseña</label>
-        </div>
-      </div>
-      <div className="center-align">
-          <a onClick={()=>{message(db)}} id="loginBtn" className="waves-effect waves-light btn">LOG IN</a>
-       </div>
-     </form>
-    </div>
-    )
-=======
 import React, { Component } from 'react';
 import {
   NavLink,
@@ -90,7 +38,6 @@ class Login extends Component {
      </div>
     )
   }
->>>>>>> 6195d725e4d5e287b672dd836c4ef5d23fb511c2
 }
 
 // Default Props here
